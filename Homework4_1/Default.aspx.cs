3997634d01bd0079e5b2887cd18e2ca6e4a83e83
Ehleung <Ellery.h.leung@gmail.com>
2017-04-13 12:42:02 -0700

    update
    xml & xsd done

--- Homework4_1/Default.aspx.cs
+++ Homework4_1/Default.aspx.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Xml;
 
 namespace Homework4_1
 {
     public partial class _Default : Page
     {
+        public XmlDocument doc = new XmlDocument();
+        public XmlNode node;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            TextBox1.Text = @"temporary string url";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string url = @TextBox1.Text;
+
+            doc.Load(url);
+            node = doc.DocumentElement;
+            Label1.Text = node.NodeType.ToString();
+            TextBox2.Text = Convert.ToString(node.Name);
 
         }
     }
---
+++ hw4part1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.IO;
+
+namespace hw4part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            verifySchema();
+            easyValidate();
+        }
+
+        public static void easyValidate()
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas.Add(null, "Hotels.xsd");
+                settings.ValidationType = ValidationType.Schema;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+                settings.IgnoreWhitespace = true;
+
+                XmlReader hotel = XmlReader.Create("Hotels.xml", settings);
+                XmlDocument document = new XmlDocument();
+                document.Load(hotel);
+
+                ValidationEventHandler eventHandler = new ValidationEventHandler(validate);
+
+                document.Validate(eventHandler);
+                Console.ReadKey();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        public static void verifySchema()
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas.Add(null, "Hotels.xsd");
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationEventHandler += new ValidationEventHandler(validate);
+            settings.IgnoreWhitespace = true;
+
+            XmlReader hotel = XmlReader.Create("Hotels.xml", settings);
+
+            while (hotel.Read())
+            {
+                System.Console.WriteLine("{0}, {1}: {2} ", hotel.NodeType, hotel.Name, hotel.Value);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void validate(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+                Console.WriteLine(" Warning" + e.Message);
+            else // Error
+                Console.WriteLine(" Error message" + e.Message);
+        }
+    }
+}
